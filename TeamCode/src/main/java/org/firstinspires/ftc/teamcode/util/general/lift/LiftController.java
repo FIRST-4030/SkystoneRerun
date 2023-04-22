e52f6eb413550aaaec7c13e10fe01438d7abd716
icaras84 <icaras84@gmail.com>
2023-04-21 17:35:03 -0700

    rewrote the SlideController.java into LiftController.java, transition the system into something that can be commanded by a state sequence much easier, cleaned up a bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/general/lift/LiftController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/general/lift/LiftController.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+
 @Config
 public class LiftController {
     private DcMotor motor;

ee55bc91a90de542759897d58491f153971dff50
icaras84 <icaras84@gmail.com>
2023-04-28 16:57:04 -0700

    rr const update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -33,8 +33,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(14, 0, 0,
-           8);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(14, 0, 2,
+           getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
     // getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
 
     /*
@@ -66,7 +66,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 66.16381555093145;
+    public static double MAX_VEL = 63.942476396953374;
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = 2.3910430908203124;
     public static double MAX_ANG_ACCEL = 4.323180557189954;

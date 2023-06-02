2bf14ebe5e5e234dc49ebe5e96f48851bdb1243e
Glenaloafe <caughlandjc@gmail.com>
2023-06-02 16:49:43 -0700

    Added red/blue switching and adjusted points

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/auto/TestAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/auto/TestAutoOp.java
@@ -15,6 +15,7 @@ import org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.util.DashboardUtil;
 import org.firstinspires.ftc.teamcode.roadrunner.util.SplineConstants;
+import org.firstinspires.ftc.teamcode.util.general.input.DSController;
 import org.firstinspires.ftc.teamcode.util.general.misc.Pose2dWrapper;
 import org.firstinspires.ftc.teamcode.util.general.rrutil.drivecmdmaker.DriveCmdMaker;
 import org.firstinspires.ftc.teamcode.util.statemachine.State;
@@ -27,16 +28,22 @@ import org.firstinspires.ftc.teamcode.util.statemachine.State;
 public class TestAutoOp extends LinearOpMode {
 
     //IMPORTANT: X increases upwards, Y increases to the left
+    public DSController inputHandler;
+
 
     public static Pose2dWrapper startPose = new Pose2dWrapper(-36, -64, 1.5707);
-    public static SplineConstants PLAT_POINT = new SplineConstants( -60, -30, 1.5707,0);
+    public static Pose2dWrapper startPoseBlue = new Pose2dWrapper(-36, 64, Math.PI * 1.5);
+    public static SplineConstants PLAT_POINT = new SplineConstants( -58, -28, 1.5707,0);
     public static SplineConstants PLAT_POINT_2 = new SplineConstants( -32, -45, 3.14159,0);
     public static Pose2dWrapper bridgePose = new Pose2dWrapper(25, -45, 3.14159);
-    public static double SPLINE_MAX_VEL = 30, SPLINE_MAX_ACCEL = 30;
+    public static Pose2dWrapper bridgePoseBlue = new Pose2dWrapper(25, 45, 0);
+    public static double SPLINE_MAX_VEL = 20, SPLINE_MAX_ACCEL = 30;
     public Servo claw;
     public Servo HR;
     public Servo HL;
     public boolean hookBool;
+
+    public boolean red = true;
 
 
 
@@ -59,28 +66,41 @@ public class TestAutoOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+        inputHandler = new DSController(gamepad1);
+        inputHandler.run();
+
         claw = hardwareMap.servo.get("claw");
         HR = hardwareMap.servo.get("HR");
         HL = hardwareMap.servo.get("HL");
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        drive.setPoseEstimate(startPose.toPose2d());
+        if (inputHandler.buttonA.pressed) red = !red;
+
+        if(red){ drive.setPoseEstimate(startPose.toPose2d()); } else { drive.setPoseEstimate(startPoseBlue.toPose2d()); }
 
         Trajectory traj1 = drive.trajectoryBuilder(drive.getPoseEstimate())
-                .splineTo(new Vector2d(PLAT_POINT.x, PLAT_POINT.y), PLAT_POINT.heading, new MecanumVelocityConstraint(SPLINE_MAX_VEL, DriveConstants.TRACK_WIDTH), new ProfileAccelerationConstraint(SPLINE_MAX_ACCEL))
+                .splineTo(new Vector2d(PLAT_POINT.x, (red ? 1 : -1) * PLAT_POINT.y), (red ? 0 : Math.PI) + PLAT_POINT.heading, new MecanumVelocityConstraint(SPLINE_MAX_VEL, DriveConstants.TRACK_WIDTH), new ProfileAccelerationConstraint(SPLINE_MAX_ACCEL))
                 .build();
         Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-                .lineToLinearHeading(new Pose2dWrapper(PLAT_POINT_2.x, PLAT_POINT_2.y, PLAT_POINT_2.heading).toPose2d())
+                .lineToLinearHeading(new Pose2dWrapper(PLAT_POINT_2.x, (red ? 1 : -1) *PLAT_POINT_2.y, (red ? 0 : Math.PI) + PLAT_POINT_2.heading).toPose2d())
                 .build();
-        Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
-                .lineToLinearHeading(bridgePose.toPose2d())
-                .build();
+        Trajectory traj3;
+        if(red) {
+            traj3 = drive.trajectoryBuilder(traj2.end())
+                    .lineToLinearHeading(bridgePose.toPose2d())
+                    .build();
+        }else{
+            traj3 = drive.trajectoryBuilder(traj2.end())
+                    .lineToLinearHeading(bridgePoseBlue.toPose2d())
+                    .build();
+        }
 
 
         DashboardUtil.previewTrajectories(FtcDashboard.getInstance(), traj1, traj2, traj3);
 
         waitForStart();
 
+        handleHooks(false);
         drive.followTrajectory(traj1);
         handleHooks(true);
         sleep(500);
